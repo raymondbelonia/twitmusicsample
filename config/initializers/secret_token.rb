9a12f24c3c2586106e183a371ed5ef1a202b3558
Raymond Belonia <raymondbelonia@gmail.com>
2013-08-28 14:05:07 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twitmusicsample::Application.config.secret_key_base = '7fbed026e7c05d9a0dd29557ccf0b2442ffbb55332a4f46769c0c2d873508da3aa43ba29b183e84120c4635105e719b1b9188a2b3b362d75bd2bc0d4b9d3f363'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Twitmusicsample::Application.config.session_store :cookie_store, key: '_twitmusicsample_session'
